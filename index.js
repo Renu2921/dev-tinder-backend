5653c02fb9f75e5be192cf8d9b6b302c31a74e89
Renu Swami <Renu@Priyanshus-MacBook-Pro.local>
2025-12-17 13:43:24 +0530

    updating port

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.use("/",authValidation,userRouter)
 
 
 
-app.listen(3000, () => {
-  console.log("Server is listening the port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
